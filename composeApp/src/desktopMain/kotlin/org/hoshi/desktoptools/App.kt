8ed79f08a821157b81564e59381efe01404970f9
lvqx <lvqixing@ddpai.com>
2025-09-03 17:54:51 +0800

    :art: 去掉默认的标题栏，准备自己实现

--- composeApp/src/desktopMain/kotlin/org/hoshi/desktoptools/App.kt
+++ composeApp/src/desktopMain/kotlin/org/hoshi/desktoptools/App.kt
@@ -2,37 +2,19 @@ package org.hoshi.desktoptools
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.onClick
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import hoshidesktoptools.composeapp.generated.resources.Res
-import hoshidesktoptools.composeapp.generated.resources.ic_calendar
-import hoshidesktoptools.composeapp.generated.resources.ic_home
-import hoshidesktoptools.composeapp.generated.resources.ic_time
-import hoshidesktoptools.composeapp.generated.resources.ic_translate
+import hoshidesktoptools.composeapp.generated.resources.*
 import org.hoshi.desktoptools.page.RadixConversionPage
 import org.hoshi.desktoptools.page.Router
 import org.hoshi.desktoptools.page.TimestampConversionPage
 import org.hoshi.desktoptools.page.WhatWeekPage
+import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -46,48 +28,10 @@ fun App() {
             modifier = Modifier.fillMaxHeight().width(80.dp).background(Color.Gray)
         ) {
             Spacer(Modifier.height(20.dp))
-            val boxModifier = Modifier.fillMaxWidth()
-            val iconModifier = Modifier.padding(30.dp, 18.dp)
-            Box(
-                boxModifier.clickable { page = Router.HOME },
-                contentAlignment = Alignment.Center
-            ) {
-                Icon(
-                    painter = painterResource(Res.drawable.ic_home),
-                    "home",
-                    modifier = iconModifier
-                )
-            }
-            Box(
-                boxModifier.clickable { page = Router.WHAT_WEEK },
-                contentAlignment = Alignment.Center
-            ) {
-                Icon(
-                    painter = painterResource(Res.drawable.ic_calendar),
-                    "",
-                    modifier = iconModifier
-                )
-            }
-            Box(
-                boxModifier.clickable { page = Router.RADIX_CONVERSION },
-                contentAlignment = Alignment.Center
-            ) {
-                Icon(
-                    painter = painterResource(Res.drawable.ic_time),
-                    "",
-                    modifier = iconModifier
-                )
-            }
-            Box(
-                boxModifier.clickable { page = Router.TIMESTAMP_CONVERSION },
-                contentAlignment = Alignment.Center
-            ) {
-                Icon(
-                    painter = painterResource(Res.drawable.ic_translate),
-                    "",
-                    modifier = iconModifier
-                )
-            }
+            PageIcon({ page = Router.HOME }, Res.drawable.ic_home)
+            PageIcon({ page = Router.WHAT_WEEK }, Res.drawable.ic_calendar)
+            PageIcon({ page = Router.RADIX_CONVERSION }, Res.drawable.ic_time)
+            PageIcon({ page = Router.TIMESTAMP_CONVERSION }, Res.drawable.ic_translate)
         }
 
         when (page) {
@@ -98,4 +42,18 @@ fun App() {
         }
     }
 
+}
+
+@Composable
+fun PageIcon(onClick: () -> Unit, iconDrawableRes: DrawableResource) {
+    Box(
+        Modifier.fillMaxWidth().clickable { onClick.invoke() },
+        contentAlignment = Alignment.Center
+    ) {
+        Icon(
+            painter = painterResource(iconDrawableRes),
+            "",
+            modifier = Modifier.padding(30.dp, 18.dp)
+        )
+    }
 }
--- composeApp/src/desktopMain/kotlin/org/hoshi/desktoptools/main.kt
+++ composeApp/src/desktopMain/kotlin/org/hoshi/desktoptools/main.kt
@@ -1,5 +1,16 @@
 package org.hoshi.desktoptools
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.window.WindowDraggableArea
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import hoshidesktoptools.composeapp.generated.resources.Res
@@ -10,8 +21,19 @@ fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "HoshiDesktopTools",
-        icon = painterResource(Res.drawable.icon) // 注意这个是窗口的图标，和应用图标不同，完全两码事
+        icon = painterResource(Res.drawable.icon), // 注意这个是窗口的图标，和应用图标不同，完全两码事
+        undecorated = true, // 是否隐藏标题栏
+        resizable = false, // 窗口是否可以调节大小
     ) {
-        App()
+        Column {
+            WindowDraggableArea(modifier = Modifier.fillMaxWidth().height(20.dp).background(Color.Gray)) {
+                Row {
+                    Text("关闭", modifier = Modifier.clickable { exitApplication() })
+                    Text("最小化", modifier = Modifier.clickable {  })
+                }
+            }
+            App()
+        }
     }
+
 }
